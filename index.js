d1d775fd77cd15690f2463ecd5f9c395ffddf6bc
Sujal kumar <sujalkr1991@gmail.com>
2026-06-24 02:15:56 +0530

    dotenv not dot-env package

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config({
+  path: "./.env",
+});
+
+let myUsername = process.env.helloname;
+console.log(myUsername);
+
 console.log("Start of backend project");
+console.log("Checking if the server restart after saving this");
