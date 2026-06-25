66327062542ff83ed0eb04576b9597f28aa04c38
Sujal kumar <sujalkr1991@gmail.com>
2026-06-25 18:17:12 +0530

    Completed Lec19 setting up mongoose

---
+++ src/db/index.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+// getting-started.js
+// const mongoose = require('mongoose');
+//  Instead of above we used import
+
+// main().catch((err) => console.log(err));
+
+async function connecDB() {
+  try {
+    await mongoose.connect(
+      process.env.MONGO_URI || "mongodb://127.0.0.1:27017/test",
+      console.log("✅ MongoDB Connected"),
+    );
+  } catch (error) {
+    console.error("❌ MongoDB connection error");
+    process.exit();
+  }
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+// Arrow function of above is like this
+// const connectDB = async () =>{}
+
+export default connecDB;
