c500b9012943cda3bd3d4f2b4032a133a2c9d301
Sujal kumar <sujalkr1991@gmail.com>
2026-06-28 16:47:04 +0530

    Completed L134

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,20 @@
 // Initial boilerplate is same
 import { Router } from "express";
 import { registerUser } from "../controllers/auth.controllers.js";
+import { userRegisterValidator } from "../validators/index.js";
+import { validate } from "../middlewares/validator.middlewares.js";
 
 const router = Router();
 // Be careful path is/register and method is post not get
-router.route("/register").post(registerUser);
+// First of all we have to bring in all the data/methods which we are writing
+
+router
+  .route("/register")
+  .post(
+    userRegisterValidator(),
+    validate /*this validate is just the middleware*/,
+    registerUser,
+  ); // since req s going from a to b i.e reached to server route now waiting t be used by user registration
+// And we want to intercept in between
 
 export default router;
---
+++ src/validators/index.js
@@ -0,0 +1,28 @@
+import { body } from "express-validator";
+
+const userRegisterValidator = () => {
+  return [
+    body("email")
+      .trim()
+      .notEmpty() //What happens in the case of it's being empty
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Email is invalid"),
+
+    // Now validating another field
+    body("username")
+      .trim()
+      .notEmpty()
+      .withMessage("Username is required")
+      .isLowercase()
+      .withMessage("Username must be in lower case")
+      .isLength({ min: 3 })
+      .withMessage("Username must be at least 3 characters long"),
+    //it's a list so don't forget about ,
+    body("password").trim().notEmpty().withMessage("Password is required"),
+
+    body("fullName").optional().trim(),
+  ];
+};
+
+export { userRegisterValidator };
